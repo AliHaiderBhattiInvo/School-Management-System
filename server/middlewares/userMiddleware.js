3227521030566de8f63c2ef61d0a20c32537ed8e
Ali Haider Bhatti <ali.haider@invozone.com>
2022-02-02 15:02:15 +0500

    express validate library integrate

--- server/middlewares/userMiddleware.js
+++ server/middlewares/userMiddleware.js
@@ -1,18 +1,30 @@
 const userModel = require("../models").Users;
+const { validationResult } = require("express-validator");
+
 // const studentModel = require("../models").Students;
 // const teacherModeel = require("../models").Teachers
 
 const validateUser = async (req, res, next) => {
-    var user = await userModel.findOne({
-        where:{
-            token: req.headers.token
-        }
-    })
-    if(user && user.role == "admin") next()
-    else res.json("You're not authorized to perform this action!")
+  var user = await userModel.findOne({
+    where: {
+      token: req.headers.token,
+    },
+  });
+  if (user && user.role == "admin") next();
+  else res.json("You're not authorized to perform this action!");
+};
 
-}
+
+const validateRequest = async (req, res, next) => {
+  const errors = validationResult(req).formatWith(({ msg }) => msg);
+  console.log("error", errors);
+  console.log("empty",!errors.isEmpty())
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  } else next();
+};
 
 module.exports = {
-    validateUser
-}
+  validateUser,
+  validateRequest,
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const router = express.Router();
-const userControllers = require("../controllers/userControllers")
-const userMiddleware = require("../middlewares/userMiddleware")
+const userControllers = require("../controllers/userControllers");
+const userMiddleware = require("../middlewares/userMiddleware");
 
+const { body } = require("express-validator");
 
-router.post("/createUser", userMiddleware.validateUser, userControllers.createUser);
+router.post(
+  "/createUser",
+  userMiddleware.validateUser,
+  [
+    body("name").isLength({ min: 3 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  userMiddleware.validateRequest,
+  userControllers.createUser
+);
 
-router.delete("/deleteUser", userMiddleware.validateUser, userControllers.deleteUser);
+router.delete(
+  "/deleteUser",
+  userMiddleware.validateUser,
+  userControllers.deleteUser
+);
 
-router.get("/fetch/students", userMiddleware.validateUser, userControllers.fetchStudents)
+router.get(
+  "/fetch/students",
+  userMiddleware.validateUser,
+  userControllers.fetchStudents
+);
 
-router.get("/fetch/teachers", userMiddleware.validateUser, userControllers.fetchTeachers)
+router.get(
+  "/fetch/teachers",
+  userMiddleware.validateUser,
+  userControllers.fetchTeachers
+);
 
-router.post("/updateUser/:id", userControllers.updateUser)
+router.post("/updateUser/:id", userControllers.updateUser);
 
 module.exports = router;
